b490d2d225a705c6742852d0a3e7cf33016ea18d
Colin Braun <electrolitic21@gmail.com>
2023-02-11 19:04:46 -0500

    Complete implementation of stop and go

--- Student Code/stop_and_go/sender.py
+++ Student Code/stop_and_go/sender.py
@@ -5,6 +5,7 @@ import sys
 # Config File
 import configparser
 
+# Header format: 32 bit sequence number (indiciating the number assigned to first byte in packet)
 if __name__ == '__main__':
 	print("Sender starting up!")
 	config_path = sys.argv[1]
@@ -20,11 +21,48 @@ if __name__ == '__main__':
 	file_to_send = cfg.get('nodes', 'file_to_send')
 	max_packet_size = int(cfg.get('network', 'MAX_PACKET_SIZE'))
 
+	with open(file_to_send, "rb") as f:
+		f.seek(0, 2)
+		file_size = f.tell()
+		f.seek(0, 0)
+		print("Start sending file...")
+		seq_no_b = (0).to_bytes(4)
+		fin_b = (0).to_bytes(1)
+		# The actual monitor send function has 4 bytes of overhead itself.
+		# With 9 bytes of overhead, we can only send 13 bytes of data less than the maximum
+		while (payload := f.read(max_packet_size-13)):
+			if file_size - f.tell() == 0:
+				print("Hit end of file, setting fin to 1")
+				fin_b = (1).to_bytes(1)
+			payload_size_b = len(payload).to_bytes(4)
+			packet = seq_no_b + payload_size_b + fin_b + payload
+			timed_out = True
+			while timed_out:
+				# TODO: Possibly adjust the payload size to depend on max packet size
+				# Send the packet
+				send_monitor.send(receiver_id, packet)
+				# Wait for ACK
+				try:
+					addr, data = send_monitor.recv(max_packet_size)
+					timed_out = False
+				except:
+					# This except happens when we timeout. Resend.
+					print(f"Timed out, resending packet with seq no {int.from_bytes(seq_no_b)}")
+					timed_out = True
+				# Receiver only need respond with seq no.
+			# We have finally received the ACK for that packet. We can send the next one.
+			ack_no_b = data[:4]
+			seq_no_b = ack_no_b
+	# Done sending the file, tell the receiver we are done
+	print("Done sending file...")
+	send_monitor.send_end(receiver_id)
+
+
 	# Exchange messages!
-	print('Sender: Sending "Hello, World!" to receiver.')
-	send_monitor.send(receiver_id, b'Hello, World!')
-	addr, data = send_monitor.recv(max_packet_size)
-	print(f'Sender: Got response from id {addr}: {data}')
+	# print('Sender: Sending "Hello, World!" to receiver.')
+	# send_monitor.send(receiver_id, b'Hello, World!')
+	# addr, data = send_monitor.recv(max_packet_size)
+	# print(f'Sender: Got response from id {addr}: {data}')
 
 	# Exit! Make sure the receiver ends before the sender. send_end will stop the emulator.
-	send_monitor.send_end(receiver_id)
+	# send_monitor.send_end(receiver_id)
