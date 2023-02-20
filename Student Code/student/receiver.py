03887d822a455606a85109000ccdaacd013a9ffb
Colin Braun <electrolitic21@gmail.com>
2023-02-20 00:23:42 -0500

    Mostly working sliding window

--- Student Code/student/receiver.py
+++ Student Code/student/receiver.py
@@ -5,6 +5,11 @@ import sys
 # Config File
 import configparser
 
+def insert_packet(packet_list, packet):
+    """
+    Insert packet into the packet_list using a binary search
+    """
+
 if __name__ == '__main__':
     print("Receivier starting up!")
     config_path = sys.argv[1]
@@ -20,29 +25,79 @@ if __name__ == '__main__':
     max_packet_size = int(cfg.get('network', 'MAX_PACKET_SIZE'))
     write_location = cfg.get('receiver', 'write_location')
 
+    # with open(write_location, "wb") as f:
+    #     print("Start receiving file...")
+    #     fin = 0
+    #     # bytes_written = 0
+    #     while not fin:
+    #         addr, packet = recv_monitor.recv(max_packet_size)
+    #         # print('--------------')
+    #         # print(type(packet))
+    #         # print(type(packet[8]))
+    #         # print(packet)
+    #         # print('--------------')
+    #         seq_no = int.from_bytes(packet[:4])
+    #         payload_size = int.from_bytes(packet[4:8])
+    #         # fin = int.from_bytes(packet[8])
+    #         # This is dumb. If you get a slice, you get bytes. If you get a single item, you get an int
+    #         fin = packet[8]
+    #         # TODO: Consider situation where payload_size = 0
+    #         if seq_no == bytes_written:
+    #             f.write(packet[9:payload_size+9])
+    #             bytes_written += payload_size
+    #         # Send back an ACK
+    #         print(f"Sending ACK with ack no {bytes_written}")
+    #         recv_monitor.send(sender_id, (bytes_written).to_bytes(4))
+    nbe = 0
+    lbg = 0
+    fin = 0
+    seq_no = 0
+    seq_to_pack_map = {}
+    final_nbe = -1
+    # while fin == 0 or seq_no != nbe:
+    while True:
+        # Wait for a packet
+        addr, packet = recv_monitor.recv(max_packet_size)
+        seq_no = int.from_bytes(packet[0:4])
+        print(f"Received packet with seq no {seq_no}, expecting nbe = {nbe}")
+        packet_size = int.from_bytes(packet[4:8])
+        # fin = int.from_bytes(packet[8]) 
+        fin = packet[8]
+        if fin:
+            final_nbe = seq_no + packet_size
+        seq_to_pack_map[seq_no] = packet
+        if seq_no == nbe:
+            nbe += packet_size
+            try:
+                # Increment NBE until we find what our cummulative ack should be
+                while int.from_bytes(seq_to_pack_map[nbe][0:4]) == nbe:
+                    packet_size = int.from_bytes(seq_to_pack_map[nbe][4:8])
+                    nbe += packet_size
+            except:
+                # This happens when the while loop trys a key that does not work
+                pass
+            # nbe is now updated, send the ack
+            print(f"Sending ack with ack no {nbe}")
+            recv_monitor.send(sender_id, nbe.to_bytes(4))
+        else:
+            # We received a packet out of order, send an ack containing NBE
+            print(f"Out of order packet, sending ack with ack no {nbe}")
+            recv_monitor.send(sender_id, nbe.to_bytes(4))
+        if nbe == final_nbe:
+            break
+
+    # Once we are out of the while loop, we are done.
+    print("Done receiving file")
+    # Now write the file out
+    keys_sorted = sorted(seq_to_pack_map.keys())
     with open(write_location, "wb") as f:
-        print("Start receiving file...")
-        fin = 0
-        bytes_written = 0
-        while not fin:
-            addr, packet = recv_monitor.recv(max_packet_size)
-            # print('--------------')
-            # print(type(packet))
-            # print(type(packet[8]))
-            # print(packet)
-            # print('--------------')
-            seq_no = int.from_bytes(packet[:4])
-            payload_size = int.from_bytes(packet[4:8])
-            # fin = int.from_bytes(packet[8])
-            # This is dumb. If you get a slice, you get bytes. If you get a single item, you get an int
-            fin = packet[8]
-            # TODO: Consider situation where payload_size = 0
-            if seq_no == bytes_written:
-                f.write(packet[9:payload_size+9])
-                bytes_written += payload_size
-            # Send back an ACK
-            print(f"Sending ACK with ack no {bytes_written}")
-            recv_monitor.send(sender_id, (bytes_written).to_bytes(4))
+        for seq_no in keys_sorted:
+            packet = seq_to_pack_map[seq_no]
+            # data_size = int.from_bytes(packet[0:4]) - 9
+            f.write(packet[9:])
+        print("Done writing file")
+
+
     # After we are done receiving, it is fine to call recv_end, but keep sending acks back for the last packet.
     recv_monitor.recv_end(write_location, sender_id)
     # TODO: Keep sending acks back for last packet
--- Student Code/student/sender.py
+++ Student Code/student/sender.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 from monitor import Monitor
 import sys
+import queue
+import time
 
 # Config File
 import configparser
+
+def print_queue(q):
+    print("QUEUE START")
+    for i in range(len(q.queue)):
+        print(q.queue[i][1])
+    print("QUEUE END")
 
 # Header format: 32 bit sequence number (indiciating the number assigned to first byte in packet)
 if __name__ == '__main__':
@@ -20,6 +28,15 @@ if __name__ == '__main__':
     receiver_id = int(cfg.get('receiver', 'id'))
     file_to_send = cfg.get('nodes', 'file_to_send')
     max_packet_size = int(cfg.get('network', 'MAX_PACKET_SIZE'))
+    # window_size = int(cfg.get('sender', 'window_size'))
+    delay = float(cfg.get('network', 'PROP_DELAY'))
+    # TODO: Play around with what is a good timeout
+    TIMEOUT = delay * 2.2
+    bw = int(cfg.get('network', 'LINK_BANDWIDTH'))
+    # Set the window size to the BDP
+    window_size = int(delay * bw)
+    print(f"MAX PACKET SIZE IS: {max_packet_size}")
+    print(f"WINDOW SIZE IS: {window_size}")
 
     with open(file_to_send, "rb") as f:
         f.seek(0, 2)
@@ -28,31 +45,74 @@ if __name__ == '__main__':
         print("Start sending file...")
         seq_no_b = (0).to_bytes(4)
         fin_b = (0).to_bytes(1)
+        lbs = 0
+        lba = 0
+        time_queue = queue.Queue()
+        all_packets_sent = False
+        all_acks_received = False
+
         # The actual monitor send function has 4 bytes of overhead itself.
         # With 9 bytes of overhead, we can only send 13 bytes of data less than the maximum
         while (payload := f.read(max_packet_size-13)):
             if file_size - f.tell() == 0:
                 print("Hit end of file, setting fin to 1")
                 fin_b = (1).to_bytes(1)
-            payload_size_b = len(payload).to_bytes(4)
-            packet = seq_no_b + payload_size_b + fin_b + payload
-            timed_out = True
-            while timed_out:
-                # TODO: Possibly adjust the payload size to depend on max packet size
-                # Send the packet
-                send_monitor.send(receiver_id, packet)
-                # Wait for ACK
-                try:
-                    addr, data = send_monitor.recv(max_packet_size)
-                    timed_out = False
-                except:
-                    # This except happens when we timeout. Resend.
-                    print(f"Timed out, resending packet with seq no {int.from_bytes(seq_no_b)}")
-                    timed_out = True
-                # Receiver only need respond with seq no.
-            # We have finally received the ACK for that packet. We can send the next one.
-            ack_no_b = data[:4]
-            seq_no_b = ack_no_b
+                all_packets_sent = True
+            packet_size_b = (len(payload) + 9).to_bytes(4)
+            seq_no_b = lbs.to_bytes(4)
+            packet = seq_no_b + packet_size_b + fin_b + payload
+            print(f"First attempt to send packet with seq no {lbs}")
+            send_monitor.send(receiver_id, packet)
+            time_queue.put((time.time() + TIMEOUT, lbs, packet))
+            lbs = lbs + int.from_bytes(packet_size_b)
+            # While our effective window is <= 0, sit there receiving
+            # while (window_size - (lbs - lba) <= 0 or (all_packets_sent and not all_acks_received)) and not time_queue.empty():
+            while window_size - (lbs - lba) <= 0 or (all_packets_sent and not all_acks_received):
+                print(f"lbs: {lbs}")
+                print(f"lba: {lba}")
+                print(f"lbs - lba: {lbs - lba}")
+                # Get the next timeout. Do not block waiting for an item
+                # print(time_queue.queue)
+                # timeout, seq_no_temp, packet_temp = time_queue.get(False)
+                timeout, seq_no_temp, packet_temp = time_queue.queue[0]
+                timeout -= time.time()
+                # Sanity check that timeout is positive
+                if timeout > 0:
+                    send_monitor.socketfd.settimeout(timeout)
+                    try:
+                        # Try to receive a packet
+                        addr, data = send_monitor.recv(max_packet_size)
+                        # If we get here, we received an ack. Update lba.
+                        lba_received = int.from_bytes(data[0:4])
+                        lba = lba_received if lba_received > lba else lba
+                        print(f"Received ack with ack no {lba}")
+                        # if lba <= seq_no_temp:
+                        # Pop values until either queue is empty or we find lba
+                        # while not time_queue.empty() and time_queue.queue[0][1] != lba:
+                        while not time_queue.empty() and time_queue.queue[0][1] < lba:
+                            print(f"Popping buffered packet with seq no {time_queue.get(False)[1]}")
+                        # If we have sent all the packets and gotten the last ack -> all acks received
+                        if all_packets_sent and lba == lbs:
+                            print("Received all acks")
+                            all_acks_received = True
+                    except:
+                        # If we timeout, resend it
+                        print(f"TIMEOUT, resending seq_no: {seq_no_temp}")
+                        # print_queue(time_queue)
+                        # Pop the value out, it will be readded to the back of the queue
+                        time_queue.get(False)
+                        send_monitor.send(receiver_id, packet_temp)
+                        # Readd it to the queue
+                        time_queue.put((time.time() + TIMEOUT, seq_no_temp, packet_temp))
+                # If it wasn't positive, it has already timed out. Resend it.
+                else:
+                        print(f"Unusual timeout, resending seq_no: {seq_no_temp}")
+                        # Pop the value out, it will be readded to the back of the queue
+                        time_queue.get(False)
+                        send_monitor.send(receiver_id, packet_temp)
+                        # Readd it to the queue
+                        time_queue.put((time.time() + TIMEOUT, seq_no_temp, packet_temp))
+
     # Done sending the file, tell the receiver we are done
     print("Done sending file...")
     send_monitor.send_end(receiver_id)
